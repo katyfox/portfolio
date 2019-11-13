b24e9aad6774a11b27371f0cc43c0e65f754e959
Katy ORourke <katyshayfox@gmail.com>
2019-11-13 11:07:22 -0700

    - Used template to add a new API controller with Entity Framework components for working with BlogPosts via the API. This is just the auto-generated scaffolding, no custom changes have been made yet.

---
+++ Controllers/BlogPostsController.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using portfolio.Models;
+
+namespace portfolio.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostsController : ControllerBase
+    {
+        private readonly BlogContext _context;
+
+        public BlogPostsController(BlogContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/BlogPosts
+        [HttpGet]
+        public IEnumerable<BlogPost> GetBlogPosts()
+        {
+            return _context.BlogPosts;
+        }
+
+        // GET: api/BlogPosts/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBlogPost([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var blogPost = await _context.BlogPosts.FindAsync(id);
+
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(blogPost);
+        }
+
+        // PUT: api/BlogPosts/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBlogPost([FromRoute] int id, [FromBody] BlogPost blogPost)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != blogPost.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(blogPost).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BlogPostExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/BlogPosts
+        [HttpPost]
+        public async Task<IActionResult> PostBlogPost([FromBody] BlogPost blogPost)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.BlogPosts.Add(blogPost);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBlogPost", new { id = blogPost.ID }, blogPost);
+        }
+
+        // DELETE: api/BlogPosts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBlogPost([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var blogPost = await _context.BlogPosts.FindAsync(id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+
+            _context.BlogPosts.Remove(blogPost);
+            await _context.SaveChangesAsync();
+
+            return Ok(blogPost);
+        }
+
+        private bool BlogPostExists(int id)
+        {
+            return _context.BlogPosts.Any(e => e.ID == id);
+        }
+    }
+}
